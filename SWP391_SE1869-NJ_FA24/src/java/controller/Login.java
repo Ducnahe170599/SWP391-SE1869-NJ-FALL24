a49dd84c9e7149eafe787b9e41894493d59b353c
cuongfug <cuonghdhe170499@fpt.edu.vn>
2024-09-14 09:49:52 +0700

    Repair

--- SWP391_SE1869-NJ_FA24/src/java/controller/Login.java
+++ SWP391_SE1869-NJ_FA24/src/java/controller/Login.java
@@ -33,7 +33,7 @@ public class Login extends HttpServlet {
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -46,7 +46,7 @@ public class Login extends HttpServlet {
         }
     } 
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code">
     /** 
      * Handles the HTTP <code>GET</code> method.
      * @param request servlet request
