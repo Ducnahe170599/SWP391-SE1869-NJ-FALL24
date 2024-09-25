434664c9585144f150d100d0163971994e4b10ca
Ducnahe170599 <ducnahe170599@fpt.edu.vn>
2024-09-25 21:36:49 +0700

    Update Login.java

--- SWP391_SE1869-NJ_FA24/src/java/controller/Login.java
+++ SWP391_SE1869-NJ_FA24/src/java/controller/Login.java
@@ -15,7 +15,7 @@ public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
+        HttpSession session = request.getSession(false)
         if (session != null) {
             session.invalidate(); // Đăng xuất người dùng nếu có phiên hoạt động
         }
